7bcdee146796b606dc5ff45b360256135b76765c
Tabita Filimon <tabitafilimon@gmail.com>
2019-02-12 16:34:27 -0500

    buttons object created and app.js complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+
+const Buttons = {
+  clear: 'clear',
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  divide: '/',
+  multiply: '*',
+  subtract: '-',
+  add: '+',
+  equal: '='
+}
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+    <CalculatorDisplay buttonsOnProps={Buttons} />
+    <ActionButton buttonsOnProps={Buttons} />
+    <NumberButton buttonsOnProps={Buttons} />
     </div>
   );
 };
 
 export default App;
+
+ 
+
+
+
+
+
+
+
+
